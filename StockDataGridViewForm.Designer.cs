0de1fc75abef9d3915d2290a204e10bfd889dd74
Braulio Veloz <bzveloz4@gmail.com>
2024-06-24 17:03:54 -0400

    Add Load Stocks Button

--- StockDataGridViewForm.Designer.cs
+++ StockDataGridViewForm.Designer.cs
@@ -28,13 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.loadDataButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // loadDataButton
+            // 
+            this.loadDataButton.Location = new System.Drawing.Point(12, 415);
+            this.loadDataButton.Name = "loadDataButton";
+            this.loadDataButton.Size = new System.Drawing.Size(98, 23);
+            this.loadDataButton.TabIndex = 0;
+            this.loadDataButton.Text = "Load Stock";
+            this.loadDataButton.UseVisualStyleBackColor = true;
+            this.loadDataButton.Click += new System.EventHandler(this.loadDataButton_Click);
+            // 
+            // StockDataGridViewForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.loadDataButton);
+            this.Name = "StockDataGridViewForm";
             this.Text = "StockDataGridViewForm";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button loadDataButton;
     }
 }
 
--- StockDataGridViewForm.cs
+++ StockDataGridViewForm.cs
@@ -16,5 +16,10 @@ namespace StockDisplay
         {
             InitializeComponent();
         }
+
+        private void loadDataButton_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
