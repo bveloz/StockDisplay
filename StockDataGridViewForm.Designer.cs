edb578cb74f99abc8210a30bd35035ecd96765b9
Braulio Veloz <bzveloz4@gmail.com>
2024-06-24 17:01:25 -0400

    Rename form

--- StockDataGridViewForm.Designer.cs
+++ StockDataGridViewForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace StockDisplay
 {
-    partial class Form1
+    partial class StockDataGridViewForm
     {
         /// <summary>
         /// Required designer variable.
@@ -31,7 +31,7 @@
             this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Text = "StockDataGridViewForm";
         }
 
         #endregion
--- StockDataGridViewForm.cs
+++ StockDataGridViewForm.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace StockDisplay
 {
-    public partial class Form1 : Form
+    public partial class StockDataGridViewForm : Form
     {
-        public Form1()
+        public StockDataGridViewForm()
         {
             InitializeComponent();
         }
